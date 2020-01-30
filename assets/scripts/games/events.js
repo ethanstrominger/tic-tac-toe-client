e0d1d25a18e9e02834383ba9c27b11c8b89e9444
Ethan Strominger <ethanstrominger@S0106602ad06f0bbf.ed.shawcable.net>
2020-01-30 17:17:06 -0500

    Change current player from X to O or O to X
    Added code so when you click on cell, it changes current player from X
    to O and vice versa and changes html of cell.

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -2,11 +2,16 @@
 // const getFormFields = require('./../../../lib/get-form-fields')
 // const api = require('./api')
 // const ui = require('./ui')
+const store = require('./../store.js')
 
 const onClick = (event) => {
   event.preventDefault()
   const cell = event.target
+  const isX = (store.currentPlayer === 'X')
+  store.currentPlayer = (isX ? 'O' : 'X')
+  $(cell).text(store.currentPlayer)
   console.log(cell)
+  console.log(store.currentPlayer)
   // api.signUp(signInData)
   //   .then((response) => {
   //     ui.onSignUpSuccess(response)
